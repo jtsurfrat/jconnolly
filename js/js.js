bf84827b6baaec8449acd3a32d90c5bd7fff0a25
Jenna Connolly <jjsurfgirl@Jennas-MacBook-Pro.local>
2017-06-19 17:59:09 -0700

    added css in syltle.css

---
+++ js/js.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  var twitterLink = document.getElementById('twitterL');
+  var faceLink = document.getElementById('faceL');
+
+  twitterLink.onclick = function(){
+    alert("Hello, I don't use twitter");
+  }
+  faceL.onclick = function(){
+    alert("Sorry, Facebook is too old school!");
+  }
+}
